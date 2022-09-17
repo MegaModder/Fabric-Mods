bc85dd544674f194cfe209896878a2b495ea58f7
megaw <megawega5746@gmail.com>
2022-09-17 13:43:56 -0400

    added important comment

---
+++ src/main/java/net/mega/gemsmod/GemsMod.java
@@ -0,0 +1,15 @@
+package net.mega.gemsmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GemsMod implements ModInitializer {
+	public static final String MOD_ID = "gemsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
